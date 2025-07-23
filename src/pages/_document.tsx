c5a2b0ad74f360dc476366d6684b1103f4149b4f
Willian Alves Moreno <willianam@uninove.br>
2025-07-23 19:13:48 -0300

    feat: Configurando stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -9,6 +10,12 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{
+            __html: getCssText(),
+          }}
         />
       </Head>
       <body>
---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, theme, styled, css, globalCss, keyframes, getCssText, createTheme } =
+  createStitches({
+    theme: {
+      colors: {
+        rocketseat: '#8257E6',
+      },
+    },
+  })
