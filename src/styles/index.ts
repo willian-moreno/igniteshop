ad3c658695474ae4c4b007e0c400ee0396ce627a
Willian Alves Moreno <willianam@uninove.br>
2025-07-23 19:26:03 -0300

    feat: Estilos globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,7 +4,15 @@ export const { config, theme, styled, css, globalCss, keyframes, getCssText, cre
   createStitches({
     theme: {
       colors: {
-        rocketseat: '#8257E6',
+        white: '#fff',
+
+        gray900: '#121214',
+        gray800: '#202024',
+        gray300: '#c4c4cc',
+        gray100: '#e1e1e6',
+
+        green500: '#00875f',
+        green300: '#00b37e',
       },
     },
   })
