c05412754143a3b8257f16caedf8c1472c450e09
Willian Alves Moreno <willianam@uninove.br>
2025-07-24 12:46:28 -0300

    feat: Imagens no next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Image from 'next/image'
 import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
 import { Container, Header } from '../styles/pages/app'
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="Logo Ignite Shop" />
+        <Image src={logoImg} alt="Logo Ignite Shop" />
       </Header>
       <Component {...pageProps} />
     </Container>
