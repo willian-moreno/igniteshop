8aafabea1bfc664da0f1820411067c85efa32c35
Willian Alves Moreno <willianam@uninove.br>
2025-08-05 19:20:38 -0300

    feat: Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { Container, ImageContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <>Product {JSON.stringify(query)}</>
+  return (
+    <Container>
+      <ImageContainer>
+        <Image
+          src={
+            'https://files.stripe.com/links/MDB8YWNjdF8xUnNxQzVHU0lpVzBzQXl4fGZsX3Rlc3RfZlhZeFQ3THo0OE1vNXRGc3U5SmhmN2lq002VpnLbvl'
+          }
+          width={420}
+          height={378}
+          alt=""
+        />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aut.</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </Container>
+  )
 }
