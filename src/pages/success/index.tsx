0ff249830fceca8118a76d768743af82a3bb0f46
Willian Alves Moreno <willianam@uninove.br>
2025-08-06 11:48:54 -0300

    feat: Estrutura de sucesso

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+import { Container, ImageContainer } from '../../styles/pages/success'
+
 export default function Success() {
-  return <>Success</>
+  return (
+    <Container>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Willian Moreno</strong>, sua <strong>Camiseta Beyond the Limits</strong> já
+        está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </Container>
+  )
 }
